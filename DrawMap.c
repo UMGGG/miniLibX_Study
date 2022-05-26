c43cbc5d42c9e2127410fe1720e719fa59eaaf2f
jaeyjeon <n8720432@naver.com>
2022-05-26 23:04:51 +0900

    22.05.26/2304_DrawMap

--- DrawMap.c
+++ DrawMap.c
@@ -6,47 +6,50 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 18:41:42 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2022/05/26 18:50:20 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2022/05/26 22:26:35 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "mlx/mlx.h"
 
+typedef struct s_imgparam
+{
+	void	*img;
+	int		height;
+	int		width;
+}	t_imgparam;
+
 int	main(void)
 {
-	void	*mlx;
-	void	*win;
-	void	*ground_img;
-	void	*wall_img;
-	int		img_width;
-	int		img_height;
-	int		Xloc;
-	int		Yloc;
-	int		i;
+	void		*mlx;
+	void		*win;
+	t_imgparam	ground;
+	t_imgparam	wall;
+	int			xloc;
+	int			yloc;
+	int			i;
 
-	img_width = 48;
-	img_height = 48;
-	Xloc = 0;
-	Yloc = 0;
+	xloc = 0;
+	yloc = 0;
 	i = 0;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "putImage");
-	ground_img = mlx_xpm_file_to_image(mlx, "imgs/ground.xpm", &img_width, &img_height);
-	wall_img = mlx_xpm_file_to_image(mlx, "imgs/rock.xpm", &img_width, &img_height);
-	while (i < 10)
+	win = mlx_new_window(mlx, 1000, 1000, "putImage");
+	ground.img = mlx_xpm_file_to_image(mlx, "imgs/ground.xpm", &ground.width, &ground.height);
+	wall.img = mlx_xpm_file_to_image(mlx, "imgs/rock.xpm", &wall.width, &wall.height);
+	while (i < 15)
 	{
-		mlx_put_image_to_window(mlx, win, wall_img, Xloc, Yloc);
-		Xloc += img_width;
+		mlx_put_image_to_window(mlx, win, wall.img, xloc, yloc);
+		xloc += wall.width;
 		i++;
 	}
-	Yloc += img_width;
-	Xloc = 0;
+	yloc += wall.height;
+	xloc = 0;
 	i = 0;
-	while (i < 10)
+	while (i < 15)
 	{
-		mlx_put_image_to_window(mlx, win, ground_img, Xloc, Yloc);
-		Xloc += img_width;
+		mlx_put_image_to_window(mlx, win, ground.img, xloc, yloc);
+		xloc += ground.width;
 		i++;
 	}
 	mlx_loop(mlx);
