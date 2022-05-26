d31b9e434d68e6c47d4be95e74e6699f8c015727
jaeyjeon <n8720432@naver.com>
2022-05-26 18:40:27 +0900

    22.05.26/1840_PopupImage

---
+++ PopupImage.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PopupImage.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/26 17:49:28 by jaeyjeon          #+#    #+#             */
+/*   Updated: 2022/05/26 18:39:54 by jaeyjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "mlx/mlx.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	int		img_width;
+	int		img_height;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 500, 500, "putImage");
+	img = mlx_xpm_file_to_image(mlx, "imgs/ground.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlx, win, img, 50, 50);
+	mlx_put_image_to_window(mlx, win, img, 50, 98);
+	mlx_put_image_to_window(mlx, win, img, 50, 146);
+	mlx_loop(mlx);
+	return (0);
+}
--- getKey.c
+++ getKey.c
@@ -6,7 +6,7 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:30:29 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2022/05/26 17:20:38 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:30:07 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include <stdlib.h>
 #include "mlx/mlx.h"
 
-#define KEY_ESC			53
-#define KEY_W			13
-#define KEY_S			1
+#define KEY_ESC				53
+#define KEY_W				13
+#define KEY_S				1
+#define PRESS_RED_BUTTON	17
 
 typedef struct s_param{
 	int		x;
@@ -41,7 +42,14 @@ int	key_press(int keycode, t_param *param)
 	return (0);
 }
 
-int			main(void)
+int	close(void)
+{
+	printf("press_red_button");
+	exit(0);
+	return (0);
+}
+
+int	main(void)
 {
 	void		*mlx;
 	void		*win;
@@ -49,14 +57,10 @@ int			main(void)
 
 	param_init(&param);
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "mlx_project");
-	printf("-------------------------------\n");
-	printf("'W key': Add 1 to x.\n");
-	printf("'S key': Subtract 1 from x\n");
-	printf("'ESC key': Exit this program\n");
-	printf("'Other keys': print current x \n");
+	win = mlx_new_window(mlx, 500, 500, "getKey");
 	printf("-------------------------------\n");
 	printf("Current x = 3\n");
 	mlx_key_hook(win, &key_press, &param);
+	mlx_hook(win, PRESS_RED_BUTTON, 0, &close, &param);
 	mlx_loop(mlx);
 }
